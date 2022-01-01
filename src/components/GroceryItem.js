705d4efb5bd35f7efac922da3be46de0e3d33393
vishnupsatish <vishnupavan.satish@gmail.com>
2022-01-01 16:09:17 -0500

    Make the + - buttons work

--- src/components/GroceryItem.js
+++ src/components/GroceryItem.js
@@ -5,10 +5,18 @@ import { faPlus, faMinus } from '@fortawesome/free-solid-svg-icons'
 
 
 const GroceryItem = () => {
-    const [cart, changeCart] = useState(null);
+    const [cart, changeCart] = useState(0);
+
+    const firstItem = () => {
+        changeCart(1);
+    }
 
     const itemAdded = () => {
-        changeCart(1);
+        changeCart(cart + 1);
+    }
+
+    const itemRemoved = () => {
+        changeCart(cart - 1);
     }
 
     return <Card sx={{ display: 'flex' }}>
@@ -24,22 +32,23 @@ const GroceryItem = () => {
                     <Typography sx={{ fontSize: { xs: 15, sm: 20 } }} component="div" variant="h5">
                         Bananas
                     </Typography>
-                    <Typography sx={{ fontSize: { xs: 12, sm: 17 } }}  color="text.secondary" component="div">
+                    <Typography sx={{ fontSize: { xs: 12, sm: 17 } }} color="text.secondary" component="div">
                         Mac Miller
                     </Typography>
                 </CardContent>
             </Box>
         </CardActionArea>
         <Box sx={{ flex: '1 1 0', display: 'flex', alignSelf: 'center', justifyContent: 'center' }}>
-            {/* <Button onClick={itemAdded} variant="contained">Add</Button> */}
-            <ButtonGroup size="small" variant="contained" aria-label="outlined primary button group">
-                <Button sx={{ fontSize: { xs: 10, sm: 12 }, maxWidth: { xs: '20px', sm: '30px' }, minWidth: '20px!important' }}><FontAwesomeIcon icon={faMinus} /></Button>
-                <Button sx={{ fontSize: { xs: 10, sm: 12 }, maxWidth: { xs: '20px', sm: '30px' }, minWidth: '20px!important' }} disabled>0</Button>
-                <Button sx={{ fontSize: { xs: 10, sm: 12 }, maxWidth: { xs: '20px', sm: '30px' }, minWidth: '20px!important' }}><FontAwesomeIcon icon={faPlus} /></Button>
-            </ButtonGroup>
+
+            {cart === 0
+                ? <Button onClick={firstItem} variant="contained">Add</Button>
+                : (<ButtonGroup size="small" variant="contained" aria-label="outlined primary button group">
+                    <Button onClick={itemRemoved} sx={{ fontSize: { xs: 10, sm: 12 }, maxWidth: { xs: '20px', sm: '30px' }, minWidth: '20px!important' }}><FontAwesomeIcon icon={faMinus} /></Button>
+                    <Button sx={{ fontSize: { xs: 12, sm: 15 }, maxWidth: { xs: '20px', sm: '30px' }, minWidth: '20px!important' }} disabled>{cart}</Button>
+                    <Button onClick={itemAdded} sx={{ fontSize: { xs: 10, sm: 12 }, maxWidth: { xs: '20px', sm: '30px' }, minWidth: '20px!important' }}><FontAwesomeIcon icon={faPlus} /></Button>
+                </ButtonGroup>)
+            }
         </Box>
-
-
     </Card>
 }
 
