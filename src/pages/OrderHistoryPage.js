88e536bf01880e53305dd1cd47088ca83ee5584a
vishnupsatish <vishnupavan.satish@gmail.com>
2022-01-03 23:23:47 -0500

    Add specific order page

--- src/pages/OrderHistoryPage.js
+++ src/pages/OrderHistoryPage.js
@@ -1,36 +1,9 @@
 import React, { useState } from 'react';
-import { Box } from '@mui/material';
+import { Box, Card, CardActionArea, CardContent, Typography, Chip } from '@mui/material';
 import Masonry, { ResponsiveMasonry } from "react-responsive-masonry"
-import OrderHistoryItem from '../components/OrderHistoryItem'
+import { Link } from "react-router-dom";
 import data from '../data/data';
-
-// Key is order ID, value is order details
-const orders = {
-    'fbhebf': {
-        date: 'Dec 28, 2021 7:17 PM',
-        items: 2,
-        status: "Cancelled",
-        price: 223,
-    },
-    'grrtjgr': {
-        date: 'Dec 29, 2021 7:17 PM',
-        items: 1,
-        status: "Active",
-        price: 111,
-    },
-    'rhrthht': {
-        date: 'Dec 29, 2021 7:17 PM',
-        items: 1,
-        status: "Active",
-        price: 111,
-    },
-    'yfefgrgh': {
-        date: 'Dec 29, 2021 7:17 PM',
-        items: 1,
-        status: "Active",
-        price: 111,
-    }
-}
+import orders from '../data/dummyOrders'
 
 const OrderHistoryPage = () => {
     return (
@@ -38,7 +11,27 @@ const OrderHistoryPage = () => {
             <ResponsiveMasonry columnsCountBreakPoints={{ 300: 1, 600: 2, 900: 3 }}>
                 <Masonry gutter="20px">
                     {Array.from(Object.keys(orders)).map((key, index) => (
-                        <OrderHistoryItem key={index} id={key} order={orders[key]} />
+                        <Card key={index}>
+                            <CardActionArea sx={{ display: 'flex' }} component={Link} to={`/order/${key}`}>
+                                <Box sx={{ flex: '2 1 0', display: 'flex', flexDirection: 'column', alignSelf: 'center' }}>
+                                    <CardContent>
+                                        <Typography sx={{ fontSize: { xs: 15, sm: 20 } }} component="div" variant="h5">
+                                            {orders[key].date}
+                                        </Typography>
+                                        <Typography sx={{ fontSize: { xs: 12, sm: 17 } }} color="text.secondary" component="div">
+                                            {orders[key].items} item(s)
+                                        </Typography>
+                                        <Chip variant="outlined" sx={{ fontSize: { xs: 10, sm: 12 }, marginTop: 0.5 }} label={orders[key].status} size="small" />
+                                    </CardContent>
+                                </Box>
+
+                                <Box sx={{ flex: '1 1 0', display: 'flex', alignSelf: 'center', justifyContent: 'center' }}>
+                                    <Typography color="text.secondary" sx={{ fontSize: { xs: 12, sm: 17 } }} component="div">
+                                        ₹{orders[key].price}
+                                    </Typography>
+                                </Box>
+                            </CardActionArea>
+                        </Card>
                     ))}
                 </Masonry>
             </ResponsiveMasonry>
--- src/pages/OrderPage.js
+++ src/pages/OrderPage.js
@@ -1,14 +1,123 @@
 import React, { useState } from 'react';
-import { Box } from '@mui/material';
+import { Typography, Card, CardContent, CardActionArea, CardMedia, Box, Button, ButtonGroup, Chip, AppBar, Toolbar } from '@mui/material';
 import { useParams, useNavigate } from "react-router-dom";
 import Masonry, { ResponsiveMasonry } from "react-responsive-masonry"
-import OrderHistoryItem from '../components/OrderHistoryItem'
 import data from '../data/data';
+import orders from '../data/dummyOrders'
+import fruitsVeggiesImage from '../assets/img/fruits-veggies.jpg'
+
 
 const OrderPage = () => {
     const params = useParams();
 
-    return params.key
+    if (!(params.key in orders)) {
+        return "That order does not exist"
+    }
+
+    const order = orders[params.key];
+
+    return (
+        <React.Fragment>
+            <Typography sx={{ marginTop: 2 }} variant="h5">Your order on {order.date}</Typography>
+            <Box sx={{ marginBottom: 2, marginTop: 2, paddingBottom: 8 }}>
+                <ResponsiveMasonry columnsCountBreakPoints={{ 0: 1, 700: 2, 1000: 3 }}>
+                    <Masonry gutter="20px">
+
+                        <Card sx={{ display: 'flex' }}>
+                            <CardMedia
+                                component="img"
+                                sx={{ width: 120 }}
+                                image={fruitsVeggiesImage}
+                            />
+
+                            <CardActionArea>
+                                <Box sx={{ display: 'flex', flexDirection: 'column', alignSelf: 'center' }}>
+                                    <CardContent>
+                                        <Typography sx={{ fontSize: { xs: 15, sm: 20 } }} component="div" variant="h5">
+                                            Banana - Premium
+                                        </Typography>
+                                        <Typography sx={{ fontSize: { xs: 12, sm: 17 } }} color="text.secondary" component="div">
+                                            ₹66.7
+                                        </Typography>
+                                        <Chip variant="outlined" sx={{ fontSize: { xs: 10, sm: 12 }, marginTop: 0.5 }} label="5kg" size="small" />
+                                        <Typography sx={{ fontSize: { xs: 10, sm: 15 }, marginTop: 0.5 }} color="text.secondary" component="div">
+                                            Buy 2 and get 25% off
+                                        </Typography>
+                                        <Typography sx={{ fontSize: { xs: 10, sm: 15 }, marginTop: 0.5 }} component="div">
+                                            Quantity: 3
+                                        </Typography>
+                                    </CardContent>
+                                </Box>
+                            </CardActionArea>
+                        </Card>
+
+                        <Card sx={{ display: 'flex' }}>
+                            <CardMedia
+                                component="img"
+                                sx={{ width: 120 }}
+                                image={fruitsVeggiesImage}
+                            />
+
+                            <CardActionArea>
+                                <Box sx={{ display: 'flex', flexDirection: 'column', alignSelf: 'center' }}>
+                                    <CardContent>
+                                        <Typography sx={{ fontSize: { xs: 15, sm: 20 } }} component="div" variant="h5">
+                                            Banana - Premium
+                                        </Typography>
+                                        <Typography sx={{ fontSize: { xs: 12, sm: 17 } }} color="text.secondary" component="div">
+                                            ₹66.7
+                                        </Typography>
+                                        <Chip variant="outlined" sx={{ fontSize: { xs: 10, sm: 12 }, marginTop: 0.5 }} label="5kg" size="small" />
+                                        <Typography sx={{ fontSize: { xs: 10, sm: 15 }, marginTop: 0.5 }} color="text.secondary" component="div">
+                                            Buy 2 and get 25% off
+                                        </Typography>
+                                        <Typography sx={{ fontSize: { xs: 10, sm: 15 }, marginTop: 0.5 }} component="div">
+                                            Quantity: 3
+                                        </Typography>
+                                    </CardContent>
+                                </Box>
+                            </CardActionArea>
+                        </Card>
+
+                        <Card sx={{ display: 'flex' }}>
+                            <CardMedia
+                                component="img"
+                                sx={{ width: 120 }}
+                                image={fruitsVeggiesImage}
+                            />
+
+                            <CardActionArea>
+                                <Box sx={{ display: 'flex', flexDirection: 'column', alignSelf: 'center' }}>
+                                    <CardContent>
+                                        <Typography sx={{ fontSize: { xs: 15, sm: 20 } }} component="div" variant="h5">
+                                            Banana - Premium
+                                        </Typography>
+                                        <Typography sx={{ fontSize: { xs: 12, sm: 17 } }} color="text.secondary" component="div">
+                                            ₹66.7
+                                        </Typography>
+                                        <Chip variant="outlined" sx={{ fontSize: { xs: 10, sm: 12 }, marginTop: 0.5 }} label="5kg" size="small" />
+                                        <Typography sx={{ fontSize: { xs: 10, sm: 15 }, marginTop: 0.5 }} color="text.secondary" component="div">
+                                            Buy 2 and get 25% off
+                                        </Typography>
+                                        <Typography sx={{ fontSize: { xs: 10, sm: 15 }, marginTop: 0.5 }} component="div">
+                                            Quantity: 3
+                                        </Typography>
+                                    </CardContent>
+                                </Box>
+                            </CardActionArea>
+                        </Card>
+                    </Masonry>
+                </ResponsiveMasonry>
+            </Box>
+
+            <AppBar position="fixed" color="primary" sx={{ top: 'auto', bottom: 0 }}>
+                <Toolbar>
+
+                </Toolbar>
+            </AppBar>
+
+        </React.Fragment>
+    );
 }
 
 export default OrderPage;
